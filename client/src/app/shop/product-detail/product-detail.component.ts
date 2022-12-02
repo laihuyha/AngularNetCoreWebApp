d9a9474930799f444a1771bb9e609ff04aa0fa79
Lại Huy Hà <laihuyha@gmail.com>
2022-12-02 11:28:02 +0700

    159

--- client/src/app/shop/product-detail/product-detail.component.ts
+++ client/src/app/shop/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/share/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -12,13 +13,29 @@ import { ShopService } from '../shop.service';
 export class ProductDetailComponent implements OnInit {
 
   product: IProduct;
+  quantity = 1;
 
-  constructor(private _service: ShopService, private activeRoute: ActivatedRoute, private breadCrumbServices: BreadcrumbService) {
+  constructor(private _service: ShopService, private activeRoute: ActivatedRoute, private breadCrumbServices: BreadcrumbService, private cartServices: BasketService) {
     this.breadCrumbServices.set('@productDetail', ' ');
   }
 
   ngOnInit(): void {
     this.passingProductData();
+  }
+
+  addItemToCart() {
+    this.cartServices.addToCart(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    this.quantity--;
+    if (this.quantity < 1) {
+      this.quantity = 1;
+    }
   }
 
   passingProductData() {
