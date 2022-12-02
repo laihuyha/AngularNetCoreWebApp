62fb88e9c4cda52cb0d6a7dc6e6ce7cbe42bfee0
Lại Huy Hà <laihuyha@gmail.com>
2022-12-02 17:00:39 +0700

    164 but not done yet

---
+++ Core/Models/Identity/Address.cs
@@ -0,0 +1,10 @@
+namespace Core.Models.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+    }
+}
